961e136f4e5d8407e77a10e999e8a8719b714ee1
Gabriel Dillenburg <gabriel.dillenburg@ibm.com>
2022-05-30 21:31:54 -0300

    feat: add HtmlReport

--- src/Football/composition/index.ts
+++ src/Football/composition/index.ts
@@ -1,8 +1,9 @@
 import { MatchReader } from './MatchReader'
 import { CsvFileReader } from './CsvFileReader'
-import { ConsoleReport } from './reports/ConsoleReport'
+// import { ConsoleReport } from './reports/ConsoleReport'
 import { WinsAnalysis } from './analyzers/WinsAnalysis'
 import { Summary } from './Summary'
+import { HtmlReport } from './reports/HtmlReport'
 
 const csvFileReader = new CsvFileReader('../football.csv')
 const matchReader = new MatchReader(csvFileReader)
@@ -10,7 +11,7 @@ matchReader.load()
 
 const summary = new Summary(
   new WinsAnalysis('Man United'),
-  new ConsoleReport()
+  new HtmlReport()
 )
 
 summary.buildAndPrintReport(matchReader.matches)
