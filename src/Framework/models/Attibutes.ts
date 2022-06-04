773f4c7759c0a180e1577041f67a45c14785f472
Gabriel Dillenburg <gabriel.dillenburg@ibm.com>
2022-06-04 15:14:22 -0300

    refactor: add aditional type generics into attributes class

---
+++ src/Framework/models/Attibutes.ts
@@ -0,0 +1,12 @@
+
+export class Attributes <T> {
+  constructor (private readonly data: T) {}
+
+  get<K extends keyof T> (key: K): T[K] {
+    return this.data[key]
+  }
+
+  set (update: T): void {
+    Object.assign(this.data, update)
+  }
+}
