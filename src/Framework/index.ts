aaab41d7a4adea9ba2016d0530fb7668860fdca5
Gabriel Dillenburg <gabriel.dillenburg@ibm.com>
2022-06-04 12:50:30 -0300

    refactor: create Sync class to save and fetch data from the json server

--- src/Framework/index.ts
+++ src/Framework/index.ts
@@ -3,5 +3,3 @@ import { User } from './models/User'
 const user = new User({ id: 2, name: 'John', age: 30 })
 
 user.set({ name: 'New Name', age: 40 })
-
-user.save()
--- src/Framework/models/User.ts
+++ src/Framework/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios'
+import { Eventing } from './Eventing'
 
 interface UserProps {
   id?: number
@@ -7,35 +7,15 @@ interface UserProps {
 }
 
 export class User {
+  public events: Eventing = new Eventing()
+
   constructor (private readonly data: UserProps) {}
 
-  get (propName: string): UserProps {
+  get (propName: string): number | string {
     return this.data[propName]
   }
 
-  set (update: UserProps): UserProps {
-    return Object.assign(this.data, update)
-  }
-
-  fetch (): void {
-    void axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data)
-      })
-  }
-
-  save (): void {
-    const id = this.get('id')
-    if (!id) {
-      void axios.post('http://localhost:3000/users', this.data)
-        .then((response: AxiosResponse): void => {
-          this.set(response.data)
-        })
-    } else {
-      void axios.put(`http://localhost:3000/users/${id}`, this.data)
-        .then((response: AxiosResponse): void => {
-          this.set(response.data)
-        })
-    }
+  set (update: UserProps): void {
+    Object.assign(this.data, update)
   }
 }
